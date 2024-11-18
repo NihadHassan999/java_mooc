ef9829e1153727357194c905b25fb66c00d48840
NihadHassan999 <nihad99@gmail.com>
2024-11-18 15:20:24 +0530

    added example

---
+++ part1/Example.java
@@ -0,0 +1,14 @@
+package part1;
+
+/*  -> 3 things : class declaration, main method, method body 
+    -> public - makes class accessible to any other class
+    -> static - allow calling the method without creating an instance of the class
+    -> packages are used to avoid namespace conflicts and group related classes and interfaces
+*/
+
+ public class Example {
+    public static void main(String[] args){
+
+        System.out.println("Hello World !");
+    }
+ }
